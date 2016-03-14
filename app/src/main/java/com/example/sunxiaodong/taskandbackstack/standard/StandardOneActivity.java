9013fdda5c935bfd4d32900dacde7ef7005e0a32
sunxiaodongme <hys_521@126.com>
2016-03-14 20:25:26 +0800

    Android任务与回退栈研究
    Android任务与回退栈研究

---
+++ app/src/main/java/com/example/sunxiaodong/taskandbackstack/standard/StandardOneActivity.java
@@ -0,0 +1,81 @@
+package com.example.sunxiaodong.taskandbackstack.standard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sunxiaodong.taskandbackstack.R;
+
+/**
+ * 启动模式为：standard
+ */
+public class StandardOneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = StandardOneActivity.class.getSimpleName();
+    private static final String SXD = "sxd";
+
+    private Button mGoStandardTwoBtn;
+    private Button mGoSingleTopOneBtn;
+    private Button mGoSingleTaskOneBtn;
+    private Button mGoSingleInstanceOneBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.standard_one_activity);
+        initView();
+        Log.i(SXD, TAG + "--taskId:" + getTaskId());//获取当前Activity所在Task的id
+    }
+
+    private void initView() {
+        mGoStandardTwoBtn = (Button) findViewById(R.id.go_standard_two);
+        mGoStandardTwoBtn.setOnClickListener(this);
+
+        mGoSingleTopOneBtn = (Button) findViewById(R.id.go_singletop_one);
+        mGoSingleTopOneBtn.setOnClickListener(this);
+
+        mGoSingleTaskOneBtn = (Button) findViewById(R.id.go_singletask_one);
+        mGoSingleTaskOneBtn.setOnClickListener(this);
+
+        mGoSingleInstanceOneBtn = (Button) findViewById(R.id.go_singleinstance_one);
+        mGoSingleInstanceOneBtn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.go_standard_two:
+                goStandardTwo();
+                break;
+            case R.id.go_singletop_one:
+                goSingleTopOne();
+                break;
+            case R.id.go_singletask_one:
+                goSingleTaskOne();
+                break;
+            case R.id.go_singleinstance_one:
+                goSingleInstanceOne();
+                break;
+        }
+    }
+
+    private void goStandardTwo() {
+
+    }
+
+    private void goSingleTopOne() {
+
+    }
+
+    private void goSingleTaskOne() {
+
+    }
+
+    private void goSingleInstanceOne() {
+
+    }
+
+}
